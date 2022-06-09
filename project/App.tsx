b46140c7455fa569d27f9c1002937dd8679c97a1
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-09 19:52:52 +0200

    Create TimeContext

--- project/App.tsx
+++ project/App.tsx
@@ -5,15 +5,19 @@ import * as SplashScreen from 'expo-splash-screen';
 
 // Internal dependencies
 import Router from './Router';
+import { getTimezones } from './misc';
+import { TimeContext } from './contexts/Time';
 
 export default function App() {
 	const [isReady, setIsReady] = useState(false);
+	const [allTimezones, setAllTimezones] = useState([]);
 
 	useEffect(() => {
 		(async () => {
 			try {
 				// Splash screen is shown until all async tasks are done.
 				SplashScreen.preventAutoHideAsync();
+				setAllTimezones(await getTimezones());
 			} catch (e) {
 				// An error occurred, notify user
 				Alert.alert('Something went wrong', 'Please try again later', [
@@ -28,5 +32,10 @@ export default function App() {
 	}, []);
 
 	if (!isReady) return null;
-	else return <Router />;
+	else
+		return (
+			<TimeContext.Provider value={{ allTimezones, selectedTimezones: [] }}>
+				<Router />
+			</TimeContext.Provider>
+		);
 }
--- project/screens/Home.tsx
+++ project/screens/Home.tsx
@@ -1,7 +1,11 @@
 // Dependencies
 import { StyleSheet, Text, View } from 'react-native';
+import { useContext } from 'react';
+import { TimeContext } from '../contexts/Time';
 
 export default function App() {
+	const { allTimezones, selectedTimezones } = useContext(TimeContext);
+
 	return (
 		<View style={styles.container}>
 			<Text>Hello World!</Text>
