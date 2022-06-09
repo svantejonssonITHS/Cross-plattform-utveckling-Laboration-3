80cc9d9174c6b134a051fb88ce7fcdc4d746e38d
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-09 21:50:21 +0200

    Add icons to tab navigation

--- project/Router.tsx
+++ project/Router.tsx
@@ -1,21 +1,40 @@
 // External dependencies
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 
 // Internal dependencies
-import Home from './screens/Home';
+import WorldClocks from './screens/WorldClocks';
 
 export default function Router() {
 	const Tab = createBottomTabNavigator();
 	return (
 		<NavigationContainer>
 			<Tab.Navigator
-				initialRouteName="Home"
-				screenOptions={{
-					headerShown: false
-				}}
+				initialRouteName="WorldClocks"
+				screenOptions={({ route }) => ({
+					headerShown: false,
+					tabBarStyle: {
+						backgroundColor: '#0073e6'
+					},
+					tabBarActiveTintColor: '#ff7d2d',
+					tabBarInactiveTintColor: '#fff',
+					tabBarIcon: ({ focused }) => {
+						let name;
+						let color;
+
+						if (route.name === 'World clocks') name = 'language';
+						else name = 'warning';
+
+						if (focused) color = '#ff7d2d';
+						else color = '#fff';
+
+						return <MaterialIcons name={name} size={24} color={color} />;
+					}
+				})}
 			>
-				<Tab.Screen name="Home" component={Home} />
+				<Tab.Screen name="World clocks" component={WorldClocks} />
+				<Tab.Screen name="Test" component={WorldClocks} />
 			</Tab.Navigator>
 		</NavigationContainer>
 	);
