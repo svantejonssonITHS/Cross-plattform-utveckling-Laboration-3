bc2d07fac08c5a323909c808c68e0e515c5cc344
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-09 17:59:21 +0200

    Add control of splash screen

--- project/App.tsx
+++ project/App.tsx
@@ -1,6 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, BackHandler, Alert, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+	const [isReady, setIsReady] = useState(false);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				// Splash screen is shown until all async tasks are done.
+				SplashScreen.preventAutoHideAsync();
+			} catch (e) {
+				// An error occurred, notify user
+				Alert.alert('Something went wrong', 'Please try again later', [
+					{ text: 'OK', onPress: () => BackHandler.exitApp() }
+				]);
+			} finally {
+				// App is ready to render, hide splash screen
+				setIsReady(true);
+				SplashScreen.hideAsync();
+			}
+		})();
+	}, []);
+
+	if (!isReady) {
+		return null;
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text>Hello World!</Text>
