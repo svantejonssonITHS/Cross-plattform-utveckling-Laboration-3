fa4d67a0be2e05f87656124ef7eddeaf0beba543
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-09 21:50:07 +0200

    Add header to WorldClocks.tsx

---
+++ project/screens/WorldClocks.tsx
@@ -0,0 +1,21 @@
+// External dependencies
+import { View } from 'react-native';
+import { useContext } from 'react';
+import { Appbar } from 'react-native-paper';
+
+// Internal dependencies
+import { TimeContext } from '../contexts/Time';
+
+export default function App() {
+	const { allTimezones, selectedTimezones } = useContext(TimeContext);
+
+	return (
+		<View style={{ backgroundColor: '#e6f3ff', height: '100%' }}>
+			<Appbar.Header style={{ backgroundColor: '#0073e6' }}>
+				<Appbar.Action icon="clock-edit-outline" />
+				<Appbar.Content title="World clocks" />
+				<Appbar.Action icon="plus" />
+			</Appbar.Header>
+		</View>
+	);
+}
