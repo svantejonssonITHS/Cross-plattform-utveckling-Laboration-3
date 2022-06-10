f798f4196e5a794c83858e213b3e1c015e4415ea
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-10 20:43:23 +0200

    Update index.ts

--- project/misc/index.ts
+++ project/misc/index.ts
@@ -11,5 +11,32 @@ export async function getTimezones() {
 
 export async function getDetailedTimezone(timezone: string) {
 	const request = await fetch(`http://worldtimeapi.org/api/timezone/${timezone.replace(/ /g, '_')}`);
-	return await request.json();
+	const detailedTimezone = await request.json();
+	detailedTimezone.timezone = detailedTimezone.timezone.replace(/_/g, ' ');
+	return detailedTimezone;
+}
+
+export function getTime(offset: number) {
+	if (!offset) return null;
+
+	// Clock offset to Greenwich Mean Time in hours
+	const gmtOffset = 3600000 * offset;
+	// Device offset to Greenwich Mean Time in hours
+	const deviceOffset = new Date().getTimezoneOffset() / 60;
+
+	// Return the time in the format of "HH:mm"
+	return JSON.stringify(new Date(Date.now() + deviceOffset + gmtOffset))
+		.split('T')[1]
+		.split(':', 2)
+		.join(':');
+}
+
+export function getOffset(offset: string) {
+	if (!offset) return null;
+
+	const preparedOffset = offset.split(/(\+|-)/g)[2].split(':');
+	const parsedOffset = parseInt(preparedOffset[0]) + parseInt(preparedOffset[1]) / 60;
+
+	if (offset.split(/(\+|-)/g)[1] === '-') return parsedOffset * -1;
+	return parsedOffset;
 }
