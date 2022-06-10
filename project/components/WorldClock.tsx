3abe4dbf2ac7a976124ec219edbfc2aff0d0657a
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-10 20:44:02 +0200

    Update WorldClock.tsx

--- project/components/WorldClock.tsx
+++ project/components/WorldClock.tsx
@@ -1,18 +1,21 @@
-// Dependencies
+// External dependencies
 import { View } from 'react-native';
 import { useState, useEffect } from 'react';
 import { Card, Title, Paragraph } from 'react-native-paper';
-import dayjs from 'dayjs';
 
-export default function WorldClock(props: { startTime: Date; offset: string; city: string }) {
-	const [date, setDate] = useState(props.startTime);
+// Internal dependencies
+import { getTime, getOffset } from '../misc';
+
+export default function WorldClock(props: { startTime: string; offset: string; city: string }) {
+	const [time, setTime] = useState(props.startTime);
+
 	useEffect(() => {
 		// set interval to update time
-		const updateInterval = setInterval(() => setDate(new Date()), 1000);
+		const updateInterval = setInterval(() => setTime(getTime(getOffset(props.offset) as number) as string), 1000);
 
 		// clear interval on unmount
 		return () => clearInterval(updateInterval);
-	}, [date]);
+	}, [time]);
 
 	return (
 		<Card>
@@ -21,7 +24,7 @@ export default function WorldClock(props: { startTime: Date; offset: string; cit
 					<Paragraph>{props.offset + ' GMT'}</Paragraph>
 					<Title>{props.city}</Title>
 				</View>
-				<Title style={{ alignSelf: 'center' }}>{dayjs(date).format('HH:mm')}</Title>
+				<Title style={{ alignSelf: 'center' }}>{time}</Title>
 			</Card.Content>
 		</Card>
 	);
