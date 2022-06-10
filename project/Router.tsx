549802bd1e5f0c5e3539d78983003dd897f29898
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-10 09:46:30 +0200

    Update Router.tsx

--- project/Router.tsx
+++ project/Router.tsx
@@ -11,7 +11,7 @@ export default function Router() {
 	return (
 		<NavigationContainer>
 			<Tab.Navigator
-				initialRouteName="WorldClocks"
+				initialRouteName="World clocks"
 				screenOptions={({ route }) => ({
 					headerShown: false,
 					tabBarStyle: {
