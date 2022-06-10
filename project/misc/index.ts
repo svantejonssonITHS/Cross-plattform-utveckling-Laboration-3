84e508091a3a5ba581d11f06210eba89c35a1e5d
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-10 16:43:30 +0200

    Update fetch functions

--- project/misc/index.ts
+++ project/misc/index.ts
@@ -1,12 +1,15 @@
 export async function getTimezones() {
 	const request = await fetch('http://worldtimeapi.org/api/timezone');
-	const timezones = await request.json();
+	let timezones = await request.json();
 
 	// Remove all timezones that are not in the format of "Continent/City"
-	return timezones.filter((timezone: string) => timezone.includes('/') && !timezone.includes('Etc'));
+	timezones = timezones.filter((timezone: string) => timezone.includes('/') && !timezone.includes('Etc'));
+
+	// Replace any underscores with spaces and return the timezones
+	return timezones.map((timezone: string) => timezone.replace(/_/g, ' '));
 }
 
 export async function getDetailedTimezone(timezone: string) {
-	const request = await fetch(`http://worldtimeapi.org/api/timezone/${timezone}`);
+	const request = await fetch(`http://worldtimeapi.org/api/timezone/${timezone.replace(/ /g, '_')}`);
 	return await request.json();
 }
