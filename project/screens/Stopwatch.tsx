76375e70762d08db67b11bbae82d4af97eab3854
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-06-14 22:37:58 +0200

    Add lap functionality

--- project/screens/Stopwatch.tsx
+++ project/screens/Stopwatch.tsx
@@ -13,6 +13,7 @@ export default function Stopwatch() {
 	const [theme] = useSelector((state: any) => [state.theme]);
 	const [paused, setPaused] = useState(true);
 	const [time, setTime] = useState(0);
+	const [laps, setLaps] = useState([] as number[]);
 	const [stopwatchInterval, setStopwatchInterval] = useState(null as any);
 	let startTime = 0;
 
@@ -42,6 +43,17 @@ export default function Stopwatch() {
 		setTime(0);
 	}
 
+	function lap() {
+		if (stopwatchInterval) {
+			if (laps.length === 0) {
+				setLaps([time]);
+			} else {
+				// Add the current time to the list of laps relative to the last lap
+				setLaps([...laps, time - laps[laps.length - 1]]);
+			}
+		}
+	}
+
 	useEffect(() => {
 		setStyles(getStyles(theme));
 	}, [theme]);
@@ -62,7 +74,19 @@ export default function Stopwatch() {
 					}
 				}}
 			/>
+			<Button
+				title="Lap"
+				onPress={() => {
+					lap();
+				}}
+			/>
 			<Text style={{ color: styles.textHighEmphasis }}>{formatStopwatch(time)}</Text>
+			{laps &&
+				laps.map((lap, index) => (
+					<Text key={index} style={{ color: styles.textHighEmphasis }}>
+						{formatStopwatch(lap)}
+					</Text>
+				))}
 		</View>
 	);
 }
